9dfeb9e995e576ff28ca12378864bb56572b0052
Jake Robers <jakerobers@gmail.com>
2017-10-21 13:01:29 -0500

    more stuff

--- app.js
+++ app.js
@@ -1,81 +1,155 @@
 window.onload = function () {
-  var game = new Phaser.Game(800,600, Phaser.AUTO, "", { preload: preload, create:create, update: update});
-  var platforms;
-  var player;
-  var star;
-  var cursors;
+  var LevelOne = function (game) {};
+  LevelOne.prototype = {
+    preload: function () {
+      this.game.load.spritesheet("dude", "assets/dude.png", 32, 48);
+      this.game.load.image("sky", "assets/sky.png");
+      this.game.load.image("tile", "assets/tile_1.png");
+      this.game.load.image("star", "assets/star.png");
+    },
+    create: function() {
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+      this.game.add.sprite(0, 0, "sky");
 
-  function preload(){
-    game.load.spritesheet("dude", "assets/dude.png", 32, 48);
-    game.load.image("sky", "assets/sky.png");
-    game.load.image("tile", "assets/tile_1.png");
-    game.load.image("star", "assets/star.png");
-  }
+      this.platforms = this.game.add.group();
+      this.platforms.enableBody = true;
 
-  function create() {
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+      this.star = this.game.add.group();
+      this.star.enableBody = true;
 
-    game.add.sprite(0, 0, "sky");
+      this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    platforms = game.add.group();
-    platforms.enableBody = true;
+      var ground;
 
-    star = game.add.group();
-    star.enableBody = true;
+      // Create the platforms
+      for (var i = 0; i < 25; i++) {
+        ground = this.platforms.create(i*32, this.game.world.height - 32, "tile");
+        ground.body.immovable = true;
+      }
 
-    cursors = game.input.keyboard.createCursorKeys();
-
-    player = game.add.sprite(32, game.world.height - 150, "dude");
-    game.physics.arcade.enable(player);
-
-    createLevelOne();
-  }
-
-  function update() {
-    var hitPlatform = game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.overlap(player, star, changeLevel, null, this);
-
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown) {
-      player.body.velocity.x = -150;
-      player.animations.play("left");
-    } else if (cursors.right.isDown) {
-      player.body.velocity.x = 150;
-      player.animations.play("right");
-    } else {
-      player.animations.stop();
-      player.frame = 4;
-    }
-
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
-      player.body.velocity.y = -280;
-    }
-  }
-
-
-  function createLevelOne() {
-    // Create the platforms
-    for (var i = 0; i < 25; i++) {
-      var ground = platforms.create(i*32, game.world.height - 32, "tile");
+      ground = this.platforms.create(21*32, this.game.world.height - 5*32, "tile");
       ground.body.immovable = true;
+
+      ground = this.platforms.create(14*32, this.game.world.height - 8*32, "tile");
+
+      ground = this.platforms.create(8*32, this.game.world.height - 11*32, "tile");
+      ground.body.immovable = true;
+
+      ground = this.platforms.create(7*32, this.game.world.height - 11*32, "tile");
+      ground.body.immovable = true;
+
+      endLevelStar = this.star.create(7.5*32, this.game.world.height - 13*32, "star");
+      endLevelStar.body.gravity.y = 100;
+      endLevelStar.body.bounce = 0.2;
+
+      this.player = this.game.add.sprite(32, this.game.world.height - 150, "dude");
+      this.game.physics.arcade.enable(this.player);
+
+      this.player.body.bounce.y = 0.2;
+      this.player.body.gravity.y = 300;
+      this.player.body.collideWorldBounds = true;
+      this.player.animations.add("left", [0, 1, 2, 3], 10, true);
+      this.player.animations.add("right", [5, 6, 7, 8], 10, true);
+
+    },
+    update: function() {
+      var hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
+      this.game.physics.arcade.overlap(this.player, this.star, this.changeLevel, null, this);
+      this.game.physics.arcade.collide(this.star, this.platforms);
+
+      this.player.body.velocity.x = 0;
+
+      if (this.cursors.left.isDown) {
+        this.player.body.velocity.x = -150;
+        this.player.animations.play("left");
+      } else if (this.cursors.right.isDown) {
+        this.player.body.velocity.x = 150;
+        this.player.animations.play("right");
+      } else {
+        this.player.animations.stop();
+        this.player.frame = 4;
+      }
+
+      if (this.cursors.up.isDown && this.player.body.touching.down && hitPlatform) {
+        this.player.body.velocity.y = -280;
+      }
+    },
+
+    changeLevel: function () {
+      this.game.state.start("2");
     }
+  };
 
-    var ground1 = platforms.create(21*32, game.world.height - 5*32, "tile");
-    ground1.body.immovable = true;
+  var LevelTwo = function(game) {}
 
-    var ground2 = platforms.create(14*32, game.world.height - 8*32, "tile");
+  LevelTwo.prototype = {
+    preload: function () {
+      this.game.load.spritesheet("dude", "assets/dude.png", 32, 48);
+      this.game.load.image("sky", "assets/sky.png");
+      this.game.load.image("tile", "assets/tile_1.png");
+      this.game.load.image("star", "assets/star.png");
+    },
+    create: function() {
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+      this.game.add.sprite(0, 0, "sky");
 
-    var ground3 = platforms.create(8*32, game.world.height - 11*32, "tile");
-    ground3.body.immovable = true;
+      this.platforms = this.game.add.group();
+      this.platforms.enableBody = true;
 
-    var ground4 = platforms.create(7*32, game.world.height - 11*32, "tile");
-    ground4.body.immovable = true;
+      this.star = this.game.add.group();
+      this.star.enableBody = true;
 
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-    player.animations.add("left", [0, 1, 2, 3], 10, true);
-    player.animations.add("right", [5, 6, 7, 8], 10, true);
-  }
+      this.player = this.game.add.sprite(32, this.game.world.height - 150, "dude");
+      this.game.physics.arcade.enable(this.player);
+
+      this.player.body.bounce.y = 0.2;
+      this.player.body.gravity.y = 300;
+      this.player.body.collideWorldBounds = true;
+      this.player.animations.add("left", [0, 1, 2, 3], 10, true);
+      this.player.animations.add("right", [5, 6, 7, 8], 10, true);
+
+      this.cursors = this.game.input.keyboard.createCursorKeys();
+
+      var ground;
+
+      // Create the platforms
+      for (var i = 0; i < 25; i++) {
+        ground = this.platforms.create(i*32, this.game.world.height - 32, "tile");
+        ground.body.immovable = true;
+      }
+    },
+    update: function() {
+      var hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
+      this.game.physics.arcade.overlap(this.player, this.star, this.changeLevel, null, this);
+      this.game.physics.arcade.collide(this.star, this.platforms);
+
+      this.player.body.velocity.x = 0;
+
+      if (this.cursors.left.isDown) {
+        this.player.body.velocity.x = -150;
+        this.player.animations.play("left");
+      } else if (this.cursors.right.isDown) {
+        this.player.body.velocity.x = 150;
+        this.player.animations.play("right");
+      } else {
+        this.player.animations.stop();
+        this.player.frame = 4;
+      }
+
+      if (this.cursors.up.isDown && this.player.body.touching.down && hitPlatform) {
+        this.player.body.velocity.y = -280;
+      }
+    },
+
+    changeLevel: function () {
+      this.game.state.start("2");
+    }
+  };
+
+
+  var game = new Phaser.Game(800,600, Phaser.AUTO, "");
+  game.state.add("1", LevelOne);
+  game.state.add("2", LevelTwo);
+  game.state.start("1");
+
 }
